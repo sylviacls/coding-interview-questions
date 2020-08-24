9dc5483a1490558fb25d10bae6f39b46781e23ba
sylviacls <sylvinha@gmail.com>
2020-08-24 20:00:05 -0300

    added isHappyII - using a hashset

--- HappyNumber.java
+++ HappyNumber.java
@@ -1,20 +1,34 @@
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.*;
+import java.util.*;
 
 /**
- * Any number will be called a happy number if, after repeatedly replacing it
- *  with a number equal to the sum of the square of all of its digits, 
- * leads us to number ‘1’. All other (not-happy) numbers will never reach ‘1’. 
- * Instead, they will be stuck in a cycle of numbers which does not include ‘1’.
- * We will use the same fast & slow pointer strategy to find the cycle and once the 
- * cycle is found, we will see if the cycle is stuck on number ‘1’ to find out if the
- *  number is happy or not
+ * Leetcode: 202. Happy Number
  * 
- * Time complexity: O(logN)
- * Space complexity: O(1)
+ * A happy number is a number defined by the following process: 
+ * Starting with any positive integer, replace the number by the sum of the squares of 
+ * its digits, and repeat the process until the number equals 1 (where it will stay),
+ * or it loops endlessly in a cycle which does not include 1. 
+ * Those numbers for which this process ends in 1 are happy numbers.
+ * 
+ * Return True if n is a happy number, and False if not.
+ * Example: 
+ * Input: 19
+ * Output: true
+ * Explanation: 
+ * 1^2 + 9^2 = 82
+ * 8^2 + 2^2 = 68
+ * 6^2 + 8^2 = 100
+ * 1^2 + 0^2 + 0^2 = 1
+ * 
  */
 public class HappyNumber {
 
+    /**
+     * Approach: Two Pointers - slow/fast
+     * 
+     * Time complexity: O(logN)
+     * Space complexity: O(1)
+     */
     public static boolean isHappy(int number) {
         int slow = number;
         int fast = number;
@@ -25,9 +39,8 @@ public class HappyNumber {
         } while (slow != fast);
 
         return slow == 1;
-
     }
-    //12
+
     private static int findSquareSum(int number) {
         int sum = 0;
         int digit = 0;
@@ -39,9 +52,31 @@ public class HappyNumber {
         return sum;
     }
 
+    /**
+     * Approach: Using a HashSet
+     * 
+     * We will keep track of all calcutated "squared numbers" in a hashset
+     * If we reach a number already stored in the set, then we have a cycle.
+     * 
+     */
+    public static boolean isHappyII(int number) {
+        Set<Integer> calculatedNumbers = new HashSet<Integer>();
+        calculatedNumbers.add(number);
+
+        while (number != 1) {
+            number = findSquareSum(number);
+            if(calculatedNumbers.contains(number)) return false;
+            calculatedNumbers.add(number);
+        }
+         return true;
+    }
+
     @Test
     public void validate() {
         Assert.assertTrue(isHappy(23));
+        Assert.assertTrue(isHappyII(23));
+
         Assert.assertFalse(isHappy(12));
+        Assert.assertFalse(isHappyII(12));
     }
 }
