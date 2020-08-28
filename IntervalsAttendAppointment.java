6d66adc5ec57a3fbd4dcb125899f3e2216b08556
sylviacls <sylvinha@gmail.com>
2020-08-28 15:25:53 -0300

    change canAttendAllAppointments() input type to int[][]

--- IntervalsAttendAppointment.java
+++ IntervalsAttendAppointment.java
@@ -6,45 +6,45 @@ import org.junit.Test;
 /**
  * Given an array of intervals representing ‘N’ appointments, 
  * find out if a person can attend all the appointments.
- * Time Complexity: O(n log n) sorting
- * Space complexity: O (n) used in sorting phase
+ * 
+ * Example 1:
+ * Appointments: [[1,4], [2,5], [7,9]]
+ * Output: false
+ * Explanation: Since [1,4] and [2,5] overlap, a person cannot attend both of these appointments.
+ * 
+ * Example 2:
+ * Appointments: [[6,7], [2,4], [8,12]]
+ * Output: true
+ * Explanation: None of the appointments overlap, therefore a person can attend all of them.
  */
 public class IntervalsAttendAppointment {
     
-    public static Boolean canAttendAllAppointments(Interval[] input) {
-
-        Arrays.sort(input, (a,b)-> Integer.compare(a.start, b.start));
-
+    /**
+     * Time Complexity: O(n log n) sorting
+     * Space complexity: O (n) used in sorting phase
+     */
+    public static boolean canAttendAllAppointments(int[][] input) {
+        Arrays.sort(input, (a,b)-> Integer.compare(a[0], b[0]));
         int i = 0;
         while (i < input.length-1) {
             //with the input sorted we only have to campare the input[i].end with input[i+1]
-            if(input[i].end > input[i+1].start) {
-                return Boolean.FALSE;
+            if(input[i][1] > input[i+1][0]) {
+                return false;
             }
             i++;
         }
-        return Boolean.TRUE;
+        return true;
     }
     
     @Test
     public void validate() {
-        Interval[] intervals1 = { new Interval(1, 4), new Interval(2, 5), new Interval(7, 9) };
-        Assert.assertFalse(IntervalsAttendAppointment.canAttendAllAppointments(intervals1));
+        int[][] intervals1 = {{1, 4}, {2, 5}, {7, 9}};
+        Assert.assertFalse(canAttendAllAppointments(intervals1));
     
-        Interval[] intervals2 = { new Interval(6, 7), new Interval(2, 4), new Interval(8, 12) };
-        Assert.assertTrue(IntervalsAttendAppointment.canAttendAllAppointments(intervals2));
+        int[][] intervals2 = {{6, 7},{2, 4}, {8, 12}};
+        Assert.assertTrue(canAttendAllAppointments(intervals2));
     
-        Interval[] intervals3 = { new Interval(4, 5), new Interval(2, 3), new Interval(3, 6) };
-        Assert.assertFalse(IntervalsAttendAppointment.canAttendAllAppointments(intervals3));
-    }
-}
-
-class Interval {
-    int start;
-    int end;
-
-    public Interval(int start, int end) {
-        this.start = start;
-        this.end = end;
+        int[][] intervals3 = {{4, 5}, {2, 3}, {3, 6}};
+        Assert.assertFalse(canAttendAllAppointments(intervals3));
     }
 }
