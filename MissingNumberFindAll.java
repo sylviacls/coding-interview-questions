6d70c09c48e9103af3ccbd88a5e1403e036cdcf3
sylviacls <sylvinha@gmail.com>
2020-08-30 17:13:09 -0300

    rename findAll to findAllCyclic and add a new algo to findAll

--- MissingNumberFindAll.java
+++ MissingNumberFindAll.java
@@ -5,19 +5,50 @@ import org.junit.Assert;
 import org.junit.Test;
 
 /**
- * We are given an unsorted array containing numbers taken from the range 1 to ‘n’. 
+ * Leetcode: 448. Find All Numbers Disappeared in an Array
+ *  We are given an unsorted array containing numbers taken from the range 1 to ‘n’. 
  * The array can have duplicates, which means some numbers will be missing. 
  * Find all those missing numbers.
  * 
- * Time complexity: O(n)
- * Space complexity: O(1)
+ * Example:
+ * Input:
+ * [4,3,2,7,8,2,3,1]
+ * 
+ * Output:
+ * [5,6]
+ * 
  */
 public class MissingNumberFindAll {
 
-    public static int[] findAll(int[] input) {
+    /**
+     * Approach: Extra Array to order
+     * 
+     * Time complexity: O(n)
+     * Space complexity: O(1)
+     * 
+     * Faster and less memory usage than findAll()
+     */
+    public Integer[] findAll(int[] nums) {
+        int[] ordered = new int[nums.length];
+        List<Integer> result = new ArrayList<Integer>();
+
+        for(int i = 0; i < nums.length; i++)  {
+            ordered[nums[i]-1] = nums[i];
+        }
+        for(int i = 0; i < ordered.length; i++)  {
+            if(ordered[i] == 0) result.add(i+1); 
+        }        
+     return result.toArray(new Integer[result.size()]);   
+    }
+    /**
+     * Approach: Cyclic Sort
+     * 
+     * Time complexity: O(n)
+     * Space complexity: O(1)
+     */
+    public static Integer[] findAllCyclic(int[] input) {
 
         int start = 0;
-        //[2, 3, 1, 8, 2, 3, 5, 1]
         while(start <input.length) {
             if(input[start] != input[input[start]-1]){
                 swap(input,start,input[start]-1);
@@ -32,7 +63,7 @@ public class MissingNumberFindAll {
                 missing.add(i+1);
             }
         }
-        return missing.stream().mapToInt(i->i).toArray();
+        return missing.toArray(new Integer[missing.size()]);   
     }
 
     private static void swap(int[] input,int i, int j) {
@@ -41,16 +72,20 @@ public class MissingNumberFindAll {
         input[j] = temp;
     }
 
+
+
     @Test
     public void validate() {
-        int[] missing = MissingNumberFindAll.findAll(new int[] { 2, 3, 1, 8, 2, 3, 5, 1 });
-        Assert.assertArrayEquals(new int[]{4, 6, 7}, missing);
+        int[] input =  new int[]{ 2, 3, 1, 8, 2, 3, 5, 1 };
+        Assert.assertArrayEquals(new Integer[]{4, 6, 7}, findAll(input));
+        Assert.assertArrayEquals(new Integer[]{4, 6, 7}, findAllCyclic(input));
+    
+        input =  new int[] { 2, 4, 1, 2 };
+        Assert.assertArrayEquals(new Integer[]{3}, findAll(input));
+        Assert.assertArrayEquals(new Integer[]{3}, findAllCyclic(input));
 
-    
-        missing = MissingNumberFindAll.findAll(new int[] { 2, 4, 1, 2 });
-        Assert.assertArrayEquals(new int[]{3}, missing);
-    
-        missing = MissingNumberFindAll.findAll(new int[] { 2, 3, 2, 1 });
-        Assert.assertArrayEquals(new int[]{4}, missing);
+        input = new int[] { 2, 3, 2, 1 };
+        Assert.assertArrayEquals(new Integer[]{4}, findAll(input));
+        Assert.assertArrayEquals(new Integer[]{4}, findAllCyclic(input));
       }
 }
