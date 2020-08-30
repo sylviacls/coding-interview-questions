5b9b4bf0c012e119bf7c280a7d6df9ea93bb1936
sylviacls <sylvinha@gmail.com>
2020-08-30 16:11:14 -0300

    move info about time and space complexity to method doc

--- CyclicSort.java
+++ CyclicSort.java
@@ -6,18 +6,23 @@ import org.junit.Test;
  * was assigned a unique number from 1 to ‘n’ based on their creation sequence.
  * Numbers are unique: from 1 to n.
  * 
- * We can use the indexes as ordering indexes for each value:
- * Ex: 1 -> 0 index, 1 -> 1 index, 2 -> 3 index
+ * Write a function to sort the objects in-place on their creation sequence number in O(n)
+ * and without any extra space.
  * 
- * Time Complexity: O(n)
- * Space complexity: O(1)
  */
 
 public class CyclicSort {
 
-    // { 3, 1, 5, 4, 2 };
+    /**
+     * We can use the indexes as ordering indexes for each value:
+     *  Ex: 1 -> 0 index, 1 -> 1 index, 2 -> 3 index
+     * 
+     * Time Complexity: O(n)
+     * Space complexity: O(1)
+     */
     public static void cyclicSort(int[] input) {
         
+        // { 3, 1, 5, 4, 2 };
         int start = 0;
         while (start <input.length) {
             if(input[start] != input[input[start]-1]) {
