924b0fcd81b4c19dc7e88a9e9bf3a17957c1fa6e
sylviacls <sylvinha@gmail.com>
2020-09-07 16:25:38 -0300

    refactor connectAllSiblings

--- BinaryTreeConnectAllLevelOrderSiblings.java
+++ BinaryTreeConnectAllLevelOrderSiblings.java
@@ -3,51 +3,40 @@ import java.util.*;
 /**
  * Given a binary tree, connect each node with its level order successor. 
  * The last node of each level should point to the first node of the next level.
- * 
- * Time complexity O(N): where ‘N’ is the total number of nodes in the tree
- * Space complexity O(N): as we need to return a list containing the level order traversal. 
- *                       We will also need O(N)O(N) space for the queue. 
+ *
  */
 public class BinaryTreeConnectAllLevelOrderSiblings {
 
-    public static void connectAllSiblings(TreeNode root) {
+    /**
+     * Approach: BFS
+     * 
+     * Time complexity O(N): where ‘N’ is the total number of nodes in the tree
+     * Space complexity O(N): as we need to return a list containing the level order traversal. 
+     *                       We will also need O(N)O(N) space for the queue. 
+     */
+    public static TreeNode connectAllSiblings(TreeNode root) {
+
+        if(root == null) return root;
+
         Queue<TreeNode> queue = new LinkedList<TreeNode>();
-
         queue.offer(root);
-        TreeNode nextLevelNode =  null;
-        TreeNode previous = null;
 
-        while(!queue.isEmpty()) {
-            TreeNode current = null;
-            int levelSize = queue.size();
-            for (int i = 0; i < levelSize; i++) {
-                current = queue.poll();
-
-                //saving the first node of current level
-                if(nextLevelNode == null) {
-                    if(current.left != null) {
-                         nextLevelNode = current.left;
-                    } else if(current.right != null) {
-                        nextLevelNode = current.right;
-                    }
-                }
-                if(previous != null) {
-                    previous.next = current;
-                }
-                //adding current node's children into queue
-                if (current.left != null) {
-                    queue.offer(current.left);
-                }
-                if (current.right != null) {
-                    queue.offer(current.right);
-                }
-
-                 previous = current;
-            }
-            // reached the end of the level
-            //reseting nextLevelNode
-            nextLevelNode = null;
+        TreeNode currentNode = null;
+        TreeNode previousNode = null;
+        while (!queue.isEmpty()) {
+          currentNode = queue.poll();
+          if (previousNode != null) {
+            previousNode.next = currentNode;
+          }
+          previousNode = currentNode;
+    
+          // insert the children of current node in the queue
+          if (currentNode.left != null)
+            queue.offer(currentNode.left);
+          if (currentNode.right != null)
+            queue.offer(currentNode.right);
         }
+        return root;
     }
 
     public static void traverseLevelOrder(TreeNode root) {
