6594663bbeee7b245ef1350c247b1560659b02fd
sylviacls <sylvinha@gmail.com>
2020-10-01 16:50:42 -0300

    improve info and comments

--- InfiniteArraySearchNumber.java
+++ InfiniteArraySearchNumber.java
@@ -1,43 +1,39 @@
 import org.junit.*;
 
 /**
- * Suppose you have a sorted array of infinite numbers, 
- * how would you search an element in the array?
+ * Given an infinite sorted array (or an array with unknown size), find if a given number 
+ * ‘key’ is present in the array. Write a function to return the index of the ‘key’ if it 
+ * is present in the array, otherwise return -1.
  * 
- * If the array is infinite, that means we don’t have proper bounds to apply binary search. 
- * So in order to find position of key, first we find bounds and then apply 
- * binary search algorithm.
+ * Since it is not possible to define an array with infinite (unknown) size, you will be 
+ * provided with an interface ArrayReader to read elements of the array. 
+ * ArrayReader.get(index) will return the number at index; if the array’s size is smaller 
+ * than the index, it will return Integer.MAX_VALUE.
  * 
- * Time Complexity: O(Log N)
- *  In the first part, we keep increasing the bound’s size exponentially (double it every time) 
- *  while searching for the proper bounds. Therefore, this step will take O(logN).
- *  In the second step, we perform the binary search which will take O(logN).
- * Thus, the overall time complexity is O(logN+logN) which is asymptotically equivalent to O(logN)
+ * Example 1:
+ * Input: [4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30], key = 16
+ * Output: 6
+ * Explanation: The key is present at index '6' in the array.
  */
-class ArrayReader {
-    int[] arr;
-  
-    ArrayReader(int[] arr) {
-      this.arr = arr;
-    }
-  
-    public int get(int index) {
-      if (index >= arr.length)
-        return Integer.MAX_VALUE;
-      return arr[index];
-    }
-}
-
 public class InfiniteArraySearchNumber {
 
     /**
+     * Approach: Binary Search
+     * 
+     * If the array is infinite, that means we don’t have proper bounds to apply binary search. 
+     * So in order to find position of key, first we find bounds.
      * To find the boundaries:
      * An efficient way to find the proper bounds is to start at the beginning of the array 
      * with the bound’s size as ‘1’ and exponentially increase the bound’s size 
      * (i.e., double it) until we find the bounds that can have the key.
-     * @param reader
-     * @param key
-     * @return
+     * 
+     * Time Complexity: O(Log N): In the first part, we keep increasing the bound’s size exponentially
+     *                           (double it every time) while searching for the proper bounds. 
+     *                           Therefore, this step will take O(logN).  In the second step, 
+     *                           we perform the binary search which will take O(logN).
+     *                           Thus, the overall time complexity is O(logN+logN) which is
+     *                           asymptotically equivalent to O(logN)
+     * Space Complexity: O(1)
      */
     public static int search(ArrayReader reader, int key) {
 
@@ -80,3 +76,16 @@ public class InfiniteArraySearchNumber {
     
 }
 
+class ArrayReader {
+  int[] arr;
+
+  ArrayReader(int[] arr) {
+    this.arr = arr;
+  }
+
+  public int get(int index) {
+    if (index >= arr.length)
+      return Integer.MAX_VALUE;
+    return arr[index];
+  }
+}
