c75443627045b79cf2d76ad674db54deb892a082
sylviacls <sylvinha@gmail.com>
2020-12-06 11:19:54 -0300

    add addTwoNumbers()

---
+++ LinkedListAddNumbers.java
@@ -0,0 +1,73 @@
+/**
+ * Leetcode: 2. Add Two Numbers
+ * https://leetcode.com/problems/add-two-numbers/
+ * 
+ * You are given two non-empty linked lists representing two non-negative integers.
+ * The digits are stored in reverse order, and each of their nodes contains a single digit. 
+ * Add the two numbers and return the sum as a linked list.
+ * You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+ * 
+ * Input: l1 = [2,4,3], l2 = [5,6,4]
+ * Output: [7,0,8]
+ * Explanation: 342 + 465 = 807.
+ */
+public class LinkedListAddNumbers {
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode(0);
+        ListNode result = head;
+        int carry = 0;
+        
+        while (l1 != null || l2 != null || carry > 0) {
+            int resVal = (l1 != null? l1.value : 0) + (l2 != null? l2.value : 0) + carry;
+            result.next = new ListNode(resVal % 10);
+            carry = resVal / 10;
+            l1 = (l1 == null ? l1 : l1.next);
+            l2 = (l2 == null ? l2 : l2.next);
+            result = result.next;
+        }
+        
+        return head.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(2);
+        l1.next = new ListNode(4);
+        l1.next.next = new ListNode(3);
+
+        ListNode l2 = new ListNode(5);
+        l2.next = new ListNode(6);
+        l2.next.next = new ListNode(4);
+
+        ListNode result = addTwoNumbers(l1, l2);
+        while(result != null) {
+            System.out.print(result.value + " ");
+            result = result.next;
+        }
+        System.out.println();
+
+        l1 = new ListNode(9);
+        l1.next = new ListNode(9);
+        l1.next.next = new ListNode(9);
+        l1.next.next.next = new ListNode(9);
+        l1.next.next.next.next = new ListNode(9);
+
+        l2 = new ListNode(9);
+        l2.next = new ListNode(9);
+        l2.next.next = new ListNode(9);
+
+        result = addTwoNumbers(l1, l2);
+        while(result != null) {
+            System.out.print(result.value+ " ");
+            result = result.next;
+        }
+    }
+}
+
+class ListNode {
+    int value = 0;
+    ListNode next;
+  
+    ListNode(int value) {
+      this.value = value;
+    }
+}  
